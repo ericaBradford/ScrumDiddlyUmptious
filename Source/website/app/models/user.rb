489219c05a69cea15f1cd04f66f7f24e6d39fb91
Erica <ericaDeniseBradford@gmail.com>
2014-04-18 21:23:18 -0600

    horrible tries to fix several bugs. Decided I need to start over. Did more interviews today.

--- Source/website/app/models/user.rb
+++ Source/website/app/models/user.rb
@@ -1,37 +1,6 @@
 class User < ActiveRecord::Base
-  
-  before_save :encrypt_password
-
-  has_many :foods
-  has_many :recipes
-  has_many :users_favorites
-  has_many :recipes, through: :users_favorites
-  has_many :users_preferences
-#  has_many :foods, through :users_preferences
-  has_many :users_ratings
-  has_many :recipes, through: :users_ratings
-  
-#  validates :password, confirmation: true
-#  validates :password, :email, :userName, presence: true
-#  validates :password, length {in: 4...15, message: "Your password should be 4-15 characters long"}
-#  validates :email, uniqueness: {case_sensitive: false, message: "There is already a user with that email"}
-#  validates :email, format: {with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/ , message: "Not correct email format"}
-#  validates :userName, uniqueness {case_sensitive: false, message: "That user name is taken"}
-#  validates :username, length {in: 4...20, message: "Your user name needs to be 4-20 characters long"}
-  
-  def self.authenticate(userName, password)
-    user = find_by_userName(userName)
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    else
-      nil
-    end
-  end
-  
-  def encrypt_password
-    if password.present?
-      self.password_salt = BCrypt::Engine.generate_salt
-      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-    end
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
--- Source/website/db/schema.rb
+++ Source/website/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417204012) do
+ActiveRecord::Schema.define(version: 20140418204527) do
 
 # Could not dump table "foods" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -35,13 +35,22 @@ ActiveRecord::Schema.define(version: 20140417204012) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "userName"
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "users_favorites", force: true do |t|
     t.integer  "id_Users"
